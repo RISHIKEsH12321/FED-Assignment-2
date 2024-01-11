86542c2f514e38e7a9f282f5dd4d3c2688984525
Rishikesh <148840508+RISHIKEsH12321@users.noreply.github.com>
2024-01-11 09:55:32 +0800

    Shared Header and Footer Complete

--- js_folder/script.js
+++ js_folder/script.js
@@ -1,4 +1,47 @@
-document.addEventListener("DOMContentLoaded",function(){
-    var content = document.getElementById("nav").innerHTML;
-    document.getElementById("header").innerHTML = content;
-})
+includeHTMLHeader();
+function includeHTMLHeader() {
+  var z, i, a, file, xhttp;
+  z = document.getElementsByTagName("*");
+  for (i = 0; i < z.length; i++) {
+    if (z[i].getAttribute("w3-include-html")) {
+      a = z[i].cloneNode(false);
+      file = z[i].getAttribute("w3-include-html");
+      var xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function () {
+        if (xhttp.readyState == 4 && xhttp.status == 200) {
+          a.removeAttribute("w3-include-html");
+          a.innerHTML = xhttp.responseText;
+          z[i].parentNode.replaceChild(a, z[i]);
+          includeHTMLHeader();
+        }
+      };
+      xhttp.open("GET", file, true);
+      xhttp.send();
+      return;
+    }
+  }
+}
+
+includeHTMLFooter();
+function includeHTMLFooter() {
+  var z, i, a, file, xhttp;
+  z = document.getElementsByTagName("*");
+  for (i = 0; i < z.length; i++) {
+    if (z[i].getAttribute("w3-include-html")) {
+      a = z[i].cloneNode(false);
+      file = z[i].getAttribute("w3-include-html");
+      var xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function () {
+        if (xhttp.readyState == 4 && xhttp.status == 200) {
+          a.removeAttribute("w3-include-html");
+          a.innerHTML = xhttp.responseText;
+          z[i].parentNode.replaceChild(a, z[i]);
+          includeHTMLFooter();
+        }
+      };
+      xhttp.open("GET", file, true);
+      xhttp.send();
+      return;
+    }
+  }
+}
