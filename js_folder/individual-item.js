8e64191af2ba17dd068a1769421db87a6b99ba15
Rishikesh <148840508+RISHIKEsH12321@users.noreply.github.com>
2024-01-21 21:03:12 +0800

    Completed displaying info on individual-item.html. Still need to complete button function AddToCart() and css.

--- js_folder/individual-item.js
+++ js_folder/individual-item.js
@@ -25,6 +25,42 @@ document.addEventListener("DOMContentLoaded", function (){
             console.log(data);
             var mainItemPic = document.querySelector("#main-img");
             mainItemPic.src = '../pictures/item-pics/' + data.picture_file_name + '.jpg';
+            var name = document.getElementById("item-name");
+            name.innerText = data.name;
+            var price = document.getElementById("item-price");
+            price.innerText = "$" + parseFloat(data.price).toFixed(2);
+            var details = document.getElementById("item-details");
+            details.innerText = data.description;
+            var color_container = document.getElementById("item-colors");
+            var color_list = [];
+            if (data.colors.includes(',')){
+                var x = data.colors.split(",")
+                for (i = 0; i < x.length;i++){
+                    color_list.push(x[i]);
+                }
+            }
+            else{
+                color_list.push(data.colors);
+                console.log(2);
+            }
+
+            for (i = 0; i < color_list.length;i++){
+                var color_choice_block = document.createElement("div");
+                var color_button = document.createElement("input");
+                color_button.type = "radio";
+                color_button.name = "color_choice";
+                color_button.value = color_list[i];
+                color_button.class = "color_choice_radio_button";
+                if (i === 0){
+                    color_button.checked = true;
+                }
+                var label = document.createElement("label");
+                label.textContent = color_list[i];
+                color_choice_block.appendChild(color_button);
+                color_choice_block.appendChild(label);                
+
+                color_container.appendChild(color_choice_block);
+            }
         })
         
 
--- js_folder/item-catalogue.js
+++ js_folder/item-catalogue.js
@@ -1,9 +1,15 @@
-document.addEventListener("DOMContentLoaded", function () {
-    // What kind of interface we want at the start 
+document.addEventListener("DOMContentLoaded", function () {    
+    // Check the session storage for the stored filter value
+    var storedFilter = sessionStorage.getItem('selectedFilter');
+    if (storedFilter) {
+        // If a filter is stored, check the corresponding radio button
+        document.querySelector(`[data-value="${storedFilter}"]`).checked = true;
+    }
+
     const APIKEY = "65ab8ff7384ac111a81414ff";
 
     let settings = {
-        method: "GET", //[cher] we will use GET to retrieve info
+        method: "GET",
         headers: {
             "Content-Type": "application/json",
             "x-apikey": APIKEY,
@@ -18,9 +24,14 @@ document.addEventListener("DOMContentLoaded", function () {
         .then(response => {
             console.log(response)
             for (var i = 0; i < response.length; i++) {
-                var item = CreateItem(response[i]);                
-                itemContainer.appendChild(item); 
-                console.log("Created one item");
+                var url = new URLSearchParams(window.location.search);
+                var filterID = url.get('FilterType');
+                if (response[i].type == filterID || !(filterID) ){
+                    var item = CreateItem(response[i]);                
+                    itemContainer.appendChild(item); 
+                    console.log("Created one item");
+                }
+                
             }
         })
         
@@ -73,3 +84,33 @@ function passObjectData(event) {
     // Navigate to the TestJS.html page
     window.location.href = link.href;
 }
+function filterCreator(event) {
+    event.preventDefault();
+    var filter = event.target.dataset.value;
+
+    // Serialize the object as JSON string
+    var id = JSON.stringify(filter);
+
+    // Store the selected filter value in the session storage
+    sessionStorage.setItem('selectedFilter', filter);
+
+    // Get the current URL
+    var currentUrl = window.location.href;
+
+    // Remove any existing query string
+    var noQueryStringUrl = currentUrl.split('?')[0];
+    var newURL;
+
+    id  = id.replace(/["']/g, '');
+    if (id == "all"){
+        newURL = noQueryStringUrl;
+    }
+    else{
+        // Append the serialized object data as a query parameter to the URL
+        newURL = noQueryStringUrl + '?FilterType=' + encodeURIComponent(id);
+    }
+    
+
+    // Navigate to the new URL
+    window.location.href = newURL;
+};
